5f8392808d8273b922e822b4ede4313bcff9e29f
Thijmen Kort <101055765+ThijmenKort@users.noreply.github.com>
2023-02-09 12:35:38 +0100

    Update Client.php

--- src/Client.php
+++ src/Client.php
@@ -4,13 +4,14 @@ namespace SoerBV\Api
   
 class Client
 {
-  protected string $host;
-  protected string $apiKey;
   
-  public function __construct(string $host, string $apiKey)
+  protected string $authCode;
+  protected string $requestId;
+  
+  public function __construct(string $authCode, string $requestId)
   {
-    $this->host = $host;
-    $this->apiKey = $apiKey;
+    $this->authCode = $authcode;
+    $this->requestId = $requestId
   }
   
 }
